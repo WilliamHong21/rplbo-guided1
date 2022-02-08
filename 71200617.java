ea41668cb2f2de785e2a35a34f6a8c64b6df8035
WilliamHong21 <78341935+WilliamHong21@users.noreply.github.com>
2022-02-08 15:35:01 +0700

    Update 71200617.java

--- 71200617.java
+++ 71200617.java
@@ -1,1 +1,15 @@
-
+public class Contoh<71200617> {
+  public static void main(String args[]) {
+    int x=10;
+    int y=25;
+    int z=x+y;
+    
+    System.out.println("hasil penjumlahan"+"dari x+y = "+ Z);
+    System.out.println("hasil akar kuadrat"+"dari z = "+ Math.sqrt(z));
+    
+    String namadepan = "Antonius";
+    System.out.println("nama depan saya adalah= "+namadepan)
+    System.out.println("nama lengkap saya"+"adalah="+namadepan+"Rachmat")
+    }
+}
+    
